f6e3f76d9ec5ec41b2fa64f5efb4e9c0d0737e67
Jeff <jeff.wang@qrious.co.nz>
2016-12-06 16:30:44 +0800

    update tools/YARNtest.sh

--- resources/tools/YARNtest.sh
+++ resources/tools/YARNtest.sh
@@ -22,13 +22,13 @@ do
          # Set reducer JVM heap 
          RED_MB=`echo "($k*0.8)/1" | bc` 
 
-        $HADOOP_PATH/hadoop jar $HADOOP_MR/hadoop-examples.jar teragen \
+       time  $HADOOP_PATH/hadoop jar $HADOOP_MR/hadoop-examples.jar teragen \
                      -Dmapreduce.job.maps=$i \
                      -Dmapreduce.map.memory.mb=$k \
                      -Dmapreduce.map.java.opts.max.heap=$MAP_MB \
                      100000 /results/tg-10GB-${i}-${j}-${k} 1>tera_${i}_${j}_${k}.out 2>tera_${i}_${j}_${k}.err                       
 
-       $HADOOP_PATH/hadoop jar $HADOOP_MR/hadoop-examples.jar terasort \
+       time $HADOOP_PATH/hadoop jar $HADOOP_MR/hadoop-examples.jar terasort \
                      -Dmapreduce.job.maps=$i \
                      -Dmapreduce.job.reduces=$j \
                      -Dmapreduce.map.memory.mb=$k \
@@ -37,7 +37,7 @@ do
                      -Dmapreduce.reduce.java.opts.max.heap=$RED_MB \
 	             /results/tg-10GB-${i}-${j}-${k}  \
                      /results/ts-10GB-${i}-${j}-${k} 1>>tera_${i}_${j}_${k}.out 2>>tera_${i}_${j}_${k}.err                         
-
+        echo "mapreduce.job.maps=$i mapreduce.job.reduces=$j mapreduce.map.memory.mb=$k"
         $HADOOP_PATH/hadoop fs -rm -r -skipTrash /results/tg-10GB-${i}-${j}-${k}                         
         $HADOOP_PATH/hadoop fs -rm -r -skipTrash /results/ts-10GB-${i}-${j}-${k}                 
       done
